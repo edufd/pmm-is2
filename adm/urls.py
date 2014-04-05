2daff0a52d025e09cbbcd71c2e505389f8fd69fe
Derlis Arguello <mg908295@gmail.com>
2014-04-05 16:59:20 -0400

    actualizar

--- adm/urls.py
+++ adm/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
         url(r'^restricted/$', views.restricted, name='restricted'),
         url(r'^logout/$', views.user_logout, name='logout'),
         url(r'^profile/$', views.profile, name='profile')
+
 )

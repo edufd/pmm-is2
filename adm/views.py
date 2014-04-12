b8ccda7c46e92b4e9f19e9e2f125f3a50931d2b5
Derlis Arguello <mg908295@gmail.com>
2014-04-12 01:50:18 -0400

    arreglado lista de usuarios ordenados y solo los que no son los usuarios actuales de la session

--- adm/views.py
+++ adm/views.py
@@ -9,6 +9,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 # Create your views here.
 from adm.models import UserProfile
 
+
 def index(request):
 
     context = RequestContext(request)
@@ -29,6 +30,7 @@ def contact(request):
 def home(request):
     context = RequestContext(request)
     return render_to_response('adm/home.html', context)
+
 
 @login_required
 def register(request):
@@ -55,6 +57,7 @@ def register(request):
             {'user_form': user_form, 'profile_form': profile_form, 'registered': registered},
             context)
 
+
 def user_login(request):
     context = RequestContext(request)
     context_dict = {}
@@ -79,15 +82,18 @@ def user_login(request):
     else:
         return render_to_response('adm/user_login.html', context_dict, context)
 
+
 @login_required
 def restricted(request):
     return HttpResponse("Como estas logeado, puedes ver este texto!")
+
 
 @login_required
 def user_logout(request):
     logout(request)
 
     return HttpResponseRedirect('/adm/')
+
 
 @login_required
 def profile(request):
@@ -104,29 +110,36 @@ def profile(request):
 
     return render_to_response('adm/profile.html', context_dict, context)
 
+
 def encode_url(name):
     return name.replace(' ', '_')
+
 
 def decode_url(url):
     return url.replace('_', ' ')
 
-def get_user_list():
 
-    user_list = User.objects.all()
+def get_user_list(user_id):
+
+    user_list = User.objects.exclude(id=user_id).order_by('id')
 
     for user in user_list:
         user.url = encode_url(user.username)
     return user_list
 
+
 @login_required
 def user_list(request):
+
+    current_user = request.user
     context = RequestContext(request)
-    user_list = get_user_list()
+    user_list = get_user_list(current_user.id)
 
     context_dict= {}
     context_dict['object_list'] = user_list
 
     return render_to_response('adm/user_list.html', context_dict, context)
+
 
 @login_required
 def user_update(request, pk):
@@ -146,6 +159,7 @@ def user_update(request, pk):
         return redirect('user_list')
 
     return render_to_response('adm/user_form.html', {'userform': user_form, 'profile_form': profile_form}, context)
+
 
 @login_required
 def user_delete(request, pk):

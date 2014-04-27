6d7d322ddbdd7d993638e2e3c0b872d664b82a39
adrianatorales <adrisil800@gmail.com>
2014-04-27 12:38:57 -0400

    models1

--- pmm_is2/apps/des/models.py
+++ pmm_is2/apps/des/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class TipoItem(models.Model):
+    id_tipo_item = models.AutoField(primary_key=True)
+    nombre_tipo_item = models.CharField(max_length=200,unique=True )
+    descripcion = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.descripcion
+    class Meta:
+        db_table = 'TipoItem'
+

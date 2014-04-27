03d6f505871565c22e4b81188cf18e4035adc2e1
adrianatorales <adrisil800@gmail.com>
2014-04-27 12:42:55 -0400

    modelos

--- pmm_is2/apps/des/models.py
+++ pmm_is2/apps/des/models.py
@@ -10,3 +10,15 @@ class TipoItem(models.Model):
     class Meta:
         db_table = 'TipoItem'
 
+class TipoItemProyecto(models.Model):
+    id_tipo_item_proyecto = models.AutoField(primary_key=True)
+    nombre_tipo_item_proyecto = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=200)
+    id_tipo_item = models.ForeignKey('TipoItem')
+    id_proyecto = models.ForeignKey('Proyecto')
+
+    def __unicode__(self):
+        return self.descripcion
+    class Meta:
+        db_table = 'TipoItem_proyecto'
+        unique_together = (("id_tipo_item", " id_proyecto"),)

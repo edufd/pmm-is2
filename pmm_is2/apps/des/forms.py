58673a787a495cfec2e97b81bc93c3b2b77aa6c5
Eduardo <edudesvars@gmail.com>
2014-05-02 11:54:11 -0400

    Agregado ABM de tipo de item, tambien se hizo mas lindo los templates de listados

--- pmm_is2/apps/des/forms.py
+++ pmm_is2/apps/des/forms.py
@@ -1,3 +1,10 @@
+from django import forms
+from pmm_is2.apps.des.models import TipoItem
 
+class TipoItemForm(forms.models.ModelForm):
+
+    class Meta:
+        model = TipoItem
+        fields = ('nombre_tipo_item', 'descripcion')
 
 

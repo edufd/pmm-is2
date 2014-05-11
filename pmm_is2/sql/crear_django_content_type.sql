323480092c264375d50cfe1a9d07baacb7949c4f
daasalbion <mg908295@gmail.com>
2014-05-11 09:38:55 -0400

    pequenho detalle

--- pmm_is2/sql/crear_django_content_type.sql
+++ pmm_is2/sql/crear_django_content_type.sql
@@ -6,14 +6,16 @@ INSERT INTO pmm.django_content_type VALUES (4,'user','auth','user');
 INSERT INTO pmm.django_content_type VALUES (5,'content type','contenttypes','contenttype');
 INSERT INTO pmm.django_content_type VALUES (6,'session','sessions','session');
 INSERT INTO pmm.django_content_type VALUES (7,'site','sites','site');
-INSERT INTO pmm.django_content_type VALUES (8,'usuario','modelos','usuario');
-INSERT INTO pmm.django_content_type VALUES (9,'rol','modelos','rol');
-INSERT INTO pmm.django_content_type VALUES (10,'permiso','modelos','permiso');
-INSERT INTO pmm.django_content_type VALUES (11,'usuario rol','modelos','usuariorol');
-INSERT INTO pmm.django_content_type VALUES (12,'rol permiso','modelos','rolpermiso');
-INSERT INTO pmm.django_content_type VALUES (13,'subjects','modelos','subjects');
-INSERT INTO pmm.django_content_type VALUES (14,'student','modelos','student');
-INSERT INTO pmm.django_content_type VALUES (15,'user profile','adm','userprofile');
+INSERT INTO pmm.django_content_type VALUES (8,'user profile','adm','userprofile');
+INSERT INTO pmm.django_content_type VALUES (9,'proyecto','adm','proyecto');
+INSERT INTO pmm.django_content_type VALUES (10,'usuario proyecto','adm','usuarioproyecto');
+INSERT INTO pmm.django_content_type VALUES (11,'fase','adm','fase');
+INSERT INTO pmm.django_content_type VALUES (12,'comite','adm','comite');
+INSERT INTO pmm.django_content_type VALUES (13,'atributo','des','atributo');
+INSERT INTO pmm.django_content_type VALUES (14,'tipo item','des','tipoitem');
+INSERT INTO pmm.django_content_type VALUES (15,'atributo tipo item','des','atributotipoitem');
+INSERT INTO pmm.django_content_type VALUES (16,'tipo item proyecto','des','tipoitemproyecto');
+INSERT INTO pmm.django_content_type VALUES (17,'item','des','item');
 
 
 

592debc093da88358a89bb0d06e7b42357ff2ea0
daasalbion <mg908295@gmail.com>
2014-06-03 21:23:35 -0400

    ya funciona permisos de comite 2

--- pmm_is2/apps/adm/decorators.py
+++ pmm_is2/apps/adm/decorators.py
@@ -8,7 +8,6 @@ def not_in_admin_group(user):
     if user:
         combined_queryset = user.groups.filter(name='Administrador').exists() | \
                             user.groups.filter(name='Lider de Proyecto').exists()
-        #print combined_queryset
         valido = combined_queryset and user.is_authenticated()
     return valido
 
@@ -18,8 +17,6 @@ def can_manage_phase(view_func):
     def _wrapped_view_func(request, *args, **kwargs):
         id_fase = kwargs['pk']
         fase = Fase.objects.get(id_fase=id_fase)
-        print request.user
-        print fase.proyecto.lider_proyecto
         if not fase.proyecto.lider_proyecto == request.user:
             return redirect('/adm/')
         return view_func(request, *args, **kwargs)
@@ -30,8 +27,6 @@ def can_create_phase(view_func):
     def _wrapped_view_func(request, *args, **kwargs):
         id_proyecto = kwargs['pk']
         proyecto = Proyecto.objects.get(id_proyecto=id_proyecto)
-        print request.user
-        print proyecto.lider_proyecto
         if not proyecto.lider_proyecto == request.user:
             return redirect('/adm/proyectos/')
         elif proyecto.fase_set.all().__len__() == proyecto.numero_fases:
@@ -57,7 +52,6 @@ def can_manage_comite(view_func):
         id_comite = kwargs['pk']
         comite = get_object_or_404(Comite, pk=id_comite)
         valido = (comite.proyecto.lider_proyecto == request.user or request.user.is_superuser is True)
-        print("es valido: ", valido)
         if not valido:
             return redirect('/adm/comite_list/')
         return view_func(request, *args, **kwargs)
